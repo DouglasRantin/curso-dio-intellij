79b907b4c566c28f4bd5309972fe7c1f3bfe48ae
DouglasRantin <douglas.rantin@gmail.com>
2021-11-23 14:58:49 -0300

    criando class livro

--- src/br/com/dio/primeiroprograma.java
+++ src/br/com/dio/primeiroprograma.java
@@ -3,10 +3,16 @@ package br.com.dio;
 import br.com.dio.model.Gato;
 
 public class primeiroprograma {
+
+
     public static void main(String[] args) {
         Gato gato = new Gato();
-
         System.out.println(gato);
+
+
+        livro Livro1 = new livro("O problema dos 3 corpos", 300);
+        System.out.println();
+
 
 
         /*int a = 5;
@@ -16,4 +22,37 @@ public class primeiroprograma {
 
     }
 
+}
+class livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
 }
